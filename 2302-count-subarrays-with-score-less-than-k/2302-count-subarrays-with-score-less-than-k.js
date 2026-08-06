160960f3f8dcf8c8df810bd6b60f194497216522
Hari Priya <haripriya816141@gmail.com>
2026-08-06 23:38:09 +0530

    Time: 16 ms (20%), Space: 66.5 MB (98.46%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countSubarrays = function(nums, k) {
+    
+    let left = 0;
+    let sum = 0;
+    let count = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+
+        sum += nums[right];
+
+        while (sum * (right - left + 1) >= k) {
+            sum -= nums[left];
+            left++;
+        }
+
+        count += right - left + 1;
+    }
+
+    return count;
+};
