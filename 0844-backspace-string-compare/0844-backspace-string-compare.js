d3e48a422001c6afcb0e263095d8df93c3d7a141
Hari Priya <haripriya816141@gmail.com>
2026-08-07 00:06:15 +0530

    Time: 1 ms (63.66%), Space: 53.6 MB (81.19%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -0,0 +1,63 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+      let i = s.length - 1;
+    let j = t.length - 1;
+
+    let skipS = 0;
+    let skipT = 0;
+
+    while (i >= 0 || j >= 0) {
+
+        // Find next valid character in s
+        while (i >= 0) {
+
+            if (s[i] === '#') {
+                skipS++;
+                i--;
+            }
+            else if (skipS > 0) {
+                skipS--;
+                i--;
+            }
+            else {
+                break;
+            }
+        }
+
+        // Find next valid character in t
+        while (j >= 0) {
+
+            if (t[j] === '#') {
+                skipT++;
+                j--;
+            }
+            else if (skipT > 0) {
+                skipT--;
+                j--;
+            }
+            else {
+                break;
+            }
+        }
+
+        // Compare valid characters
+        if (i >= 0 && j >= 0) {
+
+            if (s[i] !== t[j]) {
+                return false;
+            }
+
+        } else if (i >= 0 || j >= 0) {
+            return false;
+        }
+
+        i--;
+        j--;
+    }
+
+    return true;
+};
