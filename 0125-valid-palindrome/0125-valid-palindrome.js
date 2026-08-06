6b0081064b0e8d625311f7e728cbd593907abcf3
Hari Priya <haripriya816141@gmail.com>
2026-08-06 18:56:22 +0530

    Time: 0 ms (100%), Space: 56.1 MB (89.49%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+ function isAlphaNumeric(ch) {
+  return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+}
+
+var isPalindrome = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (left < right && !isAlphaNumeric(s[left])) {
+      left++;
+    }
+
+    while (left < right && !isAlphaNumeric(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+  return true;
+};
