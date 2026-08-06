6ba667035b8cefe9d0e67a7e2031d05c36e4906a
Hari Priya <haripriya816141@gmail.com>
2026-08-07 00:10:47 +0530

    Time: 5 ms (30.49%), Space: 56.6 MB (72.13%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+       let n = arr.length;
+    let zeros = 0;
+
+    // Count zeros
+    for (let num of arr) {
+        if (num === 0) {
+            zeros++;
+        }
+    }
+
+    let i = n - 1;
+    let j = n + zeros - 1;
+
+    while (i >= 0) {
+
+        if (j < n) {
+            arr[j] = arr[i];
+        }
+
+        if (arr[i] === 0) {
+            j--;
+
+            if (j < n) {
+                arr[j] = 0;
+            }
+        }
+
+        i--;
+        j--;
+    }
+};
