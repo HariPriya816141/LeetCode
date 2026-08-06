3c8c1128795113c6acd1dce15d4c7a669063e52f
Hari Priya <haripriya816141@gmail.com>
2026-08-06 20:54:26 +0530

    Time: 41 ms (31.98%), Space: 63.2 MB (86.05%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysWithKDistinct = function(nums, k) {
+        function atMost(k) {
+        let map = new Map();
+        let left = 0;
+        let count = 0;
+
+        for (let right = 0; right < nums.length; right++) {
+
+            map.set(nums[right], (map.get(nums[right]) || 0) + 1);
+
+            while (map.size > k) {
+                map.set(nums[left], map.get(nums[left]) - 1);
+
+                if (map.get(nums[left]) === 0) {
+                    map.delete(nums[left]);
+                }
+
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+
+    return atMost(k) - atMost(k - 1);
+};
