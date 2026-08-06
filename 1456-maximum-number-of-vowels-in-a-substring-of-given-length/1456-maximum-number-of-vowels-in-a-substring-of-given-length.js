b0075fcfc7ae516dd8154fa2d4d87e067e32b008
Hari Priya <haripriya816141@gmail.com>
2026-08-06 23:12:09 +0530

    Time: 22 ms (24.95%), Space: 55.5 MB (87.15%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+     let vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let count = 0;
+
+    // Count vowels in the first window
+    for (let i = 0; i < k; i++) {
+        if (vowels.has(s[i])) {
+            count++;
+        }
+    }
+
+    let maxCount = count;
+
+    // Slide the window
+    for (let i = k; i < s.length; i++) {
+
+        // Remove left character
+        if (vowels.has(s[i - k])) {
+            count--;
+        }
+
+        // Add right character
+        if (vowels.has(s[i])) {
+            count++;
+        }
+
+        maxCount = Math.max(maxCount, count);
+    }
+
+    return maxCount;
+};
